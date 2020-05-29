83d7f0ad6f2a340016de979c844d79d194128ee8
safiraku <66114751+safiraku@users.noreply.github.com>
2020-05-29 15:59:42 +0700

    Add files via upload

---
+++ pageWithProducts.js
@@ -0,0 +1,197 @@
+'use strict';
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+const cartDOM = document.querySelector('.cart');
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
+
+if (cart.length > 0) {
+  cart.forEach(cartItem => {
+    const product = cartItem;
+    insertItemToDOM(product);
+    countCartTotal();
+
+    addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+      const productDOM = addToCartButtonDOM.parentNode;
+
+      if (productDOM.querySelector('.product__name').innerText === product.name) {
+        handleActionButtons(addToCartButtonDOM, product);
+      }
+    });
+  });
+}
+
+addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+  addToCartButtonDOM.addEventListener('click', () => {
+    const productDOM = addToCartButtonDOM.parentNode;
+    const product = {
+      image: productDOM.querySelector('.product__image').getAttribute('src'),
+      name: productDOM.querySelector('.product__name').innerText,
+      price: productDOM.querySelector('.product__price').innerText,
+      quantity: 1
+    };
+
+    const isInCart = cart.filter(cartItem => cartItem.name === product.name).length > 0;
+
+    if (!isInCart) {
+      insertItemToDOM(product);
+      cart.push(product);
+      saveCart();
+      handleActionButtons(addToCartButtonDOM, product);
+    }
+  });
+});
+
+function insertItemToDOM(product) {
+  cartDOM.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="cart__item">
+      <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+      <h3 class="cart__item__name">${product.name}</h3>
+      
+      <button class="btn btn--primary btn--small${product.quantity === 1 ? ' btn--danger' : ''}" data-action="DECREASE_ITEM">&minus;</button>
+      <h3 class="cart__item__quantity">${product.quantity}</h3>
+      <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+      <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
+    </div>
+  `
+  );
+
+  addCartFooter();
+}
+
+function handleActionButtons(addToCartButtonDOM, product) {
+  addToCartButtonDOM.innerText = 'Telah Ditambahkan';
+  addToCartButtonDOM.disabled = true;
+
+  const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+  cartItemsDOM.forEach(cartItemDOM => {
+    if (cartItemDOM.querySelector('.cart__item__name').innerText === product.name) {
+      cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => increaseItem(product, cartItemDOM));
+      cartItemDOM.querySelector('[data-action="DECREASE_ITEM"]').addEventListener('click', () => decreaseItem(product, cartItemDOM, addToCartButtonDOM));
+      cartItemDOM.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', () => removeItem(product, cartItemDOM, addToCartButtonDOM));
+    }
+  });
+}
+
+function increaseItem(product, cartItemDOM) {
+  cart.forEach(cartItem => {
+    if (cartItem.name === product.name) {
+      cartItemDOM.querySelector('.cart__item__quantity').innerText = ++cartItem.quantity;
+      cartItemDOM.querySelector('[data-action="DECREASE_ITEM"]').classList.remove('btn--danger');
+      saveCart();
+    }
+  });
+}
+
+function decreaseItem(product, cartItemDOM, addToCartButtonDOM) {
+  cart.forEach(cartItem => {
+    if (cartItem.name === product.name) {
+      if (cartItem.quantity > 1) {
+        cartItemDOM.querySelector('.cart__item__quantity').innerText = --cartItem.quantity;
+        saveCart();
+      } else {
+        removeItem(product, cartItemDOM, addToCartButtonDOM);
+      }
+
+      if (cartItem.quantity === 1) {
+        cartItemDOM.querySelector('[data-action="DECREASE_ITEM"]').classList.add('btn--danger');
+      }
+    }
+  });
+}
+
+function removeItem(product, cartItemDOM, addToCartButtonDOM) {
+  cartItemDOM.classList.add('cart__item--removed');
+  setTimeout(() => cartItemDOM.remove(), 250);
+  cart = cart.filter(cartItem => cartItem.name !== product.name);
+  saveCart();
+  addToCartButtonDOM.innerText = 'Tambah Ke Keranjang';
+  addToCartButtonDOM.disabled = false;
+
+  if (cart.length < 1) {
+    document.querySelector('.cart-footer').remove();
+  }
+}
+
+function addCartFooter() {
+  if (document.querySelector('.cart-footer') === null) {
+    cartDOM.insertAdjacentHTML(
+      'afterend',
+      `
+      <div class="cart-footer">
+        <button class="btn btn--danger" data-action="CLEAR_CART">Bersihkan Keranjang</button>
+        <button class="btn btn--primary" data-action="CHECKOUT">Bayar</button>
+      </div>
+    `
+    );
+
+    document.querySelector('[data-action="CLEAR_CART"]').addEventListener('click', () => clearCart());
+    document.querySelector('[data-action="CHECKOUT"]').addEventListener('click', () => checkout());
+  }
+}
+
+function clearCart() {
+  cartDOM.querySelectorAll('.cart__item').forEach(cartItemDOM => {
+    cartItemDOM.classList.add('cart__item--removed');
+    setTimeout(() => cartItemDOM.remove(), 250);
+  });
+
+  cart = [];
+  localStorage.removeItem('cart');
+  document.querySelector('.cart-footer').remove();
+
+  addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+    addToCartButtonDOM.innerText = 'Tambah Ke Keranjang';
+    addToCartButtonDOM.disabled = false;
+  });
+}
+
+function checkout() {
+  let waFormHTML = `
+    <form id="wa-form" action="https://api.whatsapp.com/send?">
+    <input type="hidden" name="phone" value="6281220701139">
+  `;
+
+  // cart.forEach((cartItem, index) => {
+  //   ++index;
+
+  //   var
+  //   text = `Pesanan : ${cartItem.name}\nHarga :  ${cartItem.price}\nJumlah : ${cartItem.quantity}\nHarga Total : ${parseInt(cartItem.price)*parseInt(cartItem.quantity)}\n===================================\n`;
+  //   waFormHTML += ` 
+  //     <input type="hidden" name="phone" value="6289677657045">
+  //     <input type="hidden" name="text" value="${text}">
+  //   `;
+  // });
+
+  waFormHTML += `
+      <input type="submit" value="wa">
+    </form>
+    <div class="overlay"></div>
+  `;
+
+  document.querySelector('body').insertAdjacentHTML('beforeend', waFormHTML);
+
+  var target = document.querySelector("#wa-form");
+  var template = `<input type="hidden" name="text" value="~id~">`;
+  
+  var ab = ""
+  cart.forEach(function(item) {
+    console.log(item)
+    ab += `Pesanan      : ${item.name}\nHarga         :  ${item.price}\nJumlah        : ${item.quantity}\nHarga Total : ${parseInt(item.price)*parseInt(item.quantity)}\n\n`
+    
+  });
+  target.insertAdjacentHTML("beforeend", template.replace(/~id~/g, ab));
+console.log(ab)
+  document.getElementById('wa-form').submit();
+}
+
+function countCartTotal() {
+  let cartTotal = 0;
+  cart.forEach(cartItem => (cartTotal += cartItem.quantity * cartItem.price));
+  document.querySelector('[data-action="CHECKOUT"]').innerText = `Bayar Rp.${cartTotal}`;
+}
+
+function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+  countCartTotal();
+}
